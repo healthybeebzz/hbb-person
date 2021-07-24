5298879f76268e47793d01b8f6dd76a21ecdf794
Maria Mihaila <maria.mihaila.17@gmail.com>
2021-07-24 15:56:01 +0300

    Add post and put route

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'express';
+
 const app = express();
+ 
 const port = 3000;
+
+app.use(bodyParser.json())
 
 // TODO:
 // 1. Add get person route
 // 2. Add put person route
 // 3. Add create person route
 // 4. Add delete person route
-app.get('/person/:personId', (req,res) => {
+app.get('/person/:personId', (req, res) => {
   const personId = req.params.personId;
-  
+
   const response = {
     personId,
     firstName: "Maria",
@@ -19,8 +24,26 @@ app.get('/person/:personId', (req,res) => {
   res.send(response);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post('/person/create', (req, res) => {
+  console.log("req.body ", req.body);
+
+  const response = {
+    status: "ok",
+    message: "New person created."
+  }
+
+  res.send(response);
+});
+
+app.put('/person/edit', (req, res) => {
+  console.log("req.body ", req.body);
+
+  const response = {
+    firstName: "Razvan",
+    lastName: "Pavelescu"
+  }
+
+  res.send(response);
 });
 
 app.listen(port, () => {
