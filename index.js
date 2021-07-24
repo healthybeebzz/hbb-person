1bb7722696d1855dcdc8b6f00b3b29fefcfcef59
Maria Mihaila <maria.mihaila.17@gmail.com>
2021-07-24 14:58:30 +0300

    Create person get route

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// TODO:
+// 1. Add get person route
+// 2. Add put person route
+// 3. Add create person route
+// 4. Add delete person route
+app.get('/person/:personId', (req,res) => {
+  const personId = req.params.personId;
+  
+  const response = {
+    personId,
+    firstName: "Maria",
+    lastName: "Mihaila"
+  };
+
+  res.send(response);
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
