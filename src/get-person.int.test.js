54b4f34f11e441dfbe4c15118de444b2ae002740
Maria <maria.mihaila.17@gmail.com>
2021-09-13 12:03:42 +0300

    Integration test for the get-person endpoint

---
+++ src/get-person.int.test.js
@@ -0,0 +1,37 @@
+import {expect} from 'chai';
+import request from 'supertest';
+import {createWebServer} from "./create-web-server.js";
+
+
+describe('/person/:personId', () => {
+    let port;
+    let server;
+
+    before(async () => {
+        server = createWebServer();
+        port = server.port;
+
+        await server.start();
+    });
+
+    after(async () => {
+        await server.stop();
+    });
+
+    it('given existing person > when calling get /person/:personId > should return valid response', async () => {
+        const payload = {
+            requestId: 1,
+            username: 'person123',
+            externalToken: 1
+        };
+
+        const response = await request(`http://localhost:${port}`).get('/person/1').send(payload);
+
+        expect(response.status).to.be.equal(200);
+        expect(response.body).to.be.deep.equal({
+                personId: "1",
+                firstName: "Maria",
+                lastName: "Mihaila"
+        });
+    });
+});
