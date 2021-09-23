6313f34753b6e2417c4d2a97bcead2aac07e8b59
Maria <maria.mihaila.17@gmail.com>
2021-09-23 19:35:50 +0300

    Removed build

--- build/error-handler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const errorHandler = (err, req, res, next) => {
-    console.log("err ", err);
-    res.status(500).send('Oops, something went wrong!');
-};
-exports.errorHandler = errorHandler;
-//# sourceMappingURL=error-handler.js.map
