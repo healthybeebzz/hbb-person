e3e60130c1f17c0877eb6c6b7dae26ed2a6293cf
Maria <maria.mihaila.17@gmail.com>
2021-09-25 19:42:13 +0300

    Create fetchPatient function: query to the db for the patient with given user id and map the result.

--- src/operations.ts
+++ src/operations.ts
@@ -17,3 +17,33 @@ export type Operation = {
     sex: string,
     dateOfBirth: string,
 }
+
+
+/**
+* Fetches transcations from the table and maps them to DTO (data transfer objects).
+* @param pool
+* @param userId
+*/
+export const fetchPatient = async (pool: Pool, userId: number) => {
+    const queryResult = await pool.query(`SELECT * FROM hbb_person.patients WHERE user_id=${userId}`);
+    const rows = queryResult.rows;
+
+    const resultsArray: Array<Operation> = [];
+    for (let i = 0; i < rows.length; i++) {
+        const obj = {
+            id: rows[i].id,
+            userId: rows[i].user_id,
+            firstName: rows[i].first_name,
+            lastName: rows[i].last_name,
+            emailAddress: rows[i].email_address,
+            homeAddress: rows[i].home_address,
+            sex: rows[i].sex,
+            dateOfBirth: rows[i].date_of_birth,
+            createdAt: rows[i].created_at
+        }
+
+        resultsArray.push(obj);
+    }
+
+    return resultsArray;
+}
