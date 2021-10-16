1ab0872942358e59038fad25b5add7d63a7a5172
Maria <maria.mihaila.17@gmail.com>
2021-10-16 12:03:39 +0300

    Fix delete person integration test

--- src/delete-person.int.test.ts
+++ src/delete-person.int.test.ts
@@ -1,31 +1,45 @@
 import {expect} from 'chai';
-import request from 'supertest';
+import axios from "axios";
 import {createWebServer} from "./create-web-server";
+import {connectToDb} from "./db-connect";
+import {insertOperation} from "./operations";
 
 
-describe('/person/:userId/delete', () => {
+describe('/person/2/delete', () => {
     let port: number;
     let server: { stop: () => Promise<unknown>, port: number, start: () => Promise<unknown> };
+
+    let pool = connectToDb();
 
     before(async () => {
         server = createWebServer();
         port = server.port;
 
         await server.start();
+
+        await insertOperation(pool, {
+            firstName: 'andrei',
+            lastName: 'ardei',
+            emailAddress: 'ardeiul.iute@iuteala.com',
+            homeAddress: 'strada iute nr 5',
+            sex: 'male',
+            dateOfBirth: '25.08.2000'
+        });
+
     });
 
     after(async () => {
         await server.stop();
+
     });
 
     it('given existing person > when calling delete /person/:userId/delete > should return valid response', async () => {
 
-        const response = await request(`http://localhost:${port}`).delete('/person/1/delete');
+        const response = await axios.delete(`http://localhost:${port}/person/2/delete`);
 
         expect(response.status).to.be.equal(200);
-        expect(response.body).to.be.deep.equal({
-            firstName: "Maria",
-            lastName: "Pavelescu"
-        });
+        expect(response.data).to.be.deep.equal(
+           `The user with the id 2 was deleted from the database.`
+        );
     });
 });
