da22e77a568b40f4d15c17505d800373bb31668f
Maria <maria.mihaila.17@gmail.com>
2021-10-16 13:09:08 +0300

    Fix get person integration test

--- src/get-person.int.test.ts
+++ src/get-person.int.test.ts
@@ -1,37 +1,60 @@
 import {expect} from 'chai';
-import request from 'supertest';
+import axios from "axios";
 import {createWebServer} from "./create-web-server";
+import {connectToDb} from "./db-connect";
+import {insertOperation} from "./operations";
+import {Person} from "./person-types";
 
 
-describe('/person/:personId', () => {
+describe('/person/1', () => {
     let port: number;
-    let server: { stop: () => Promise<unknown>, port: number, start: () => Promise<unknown> };;
+    let server: { stop: () => Promise<unknown>, port: number, start: () => Promise<unknown> };
+
+    let pool = connectToDb();
 
     before(async () => {
         server = createWebServer();
         port = server.port;
 
         await server.start();
+
+        await pool.query(`ALTER SEQUENCE hbb_person.patients_user_id_seq RESTART `);
+        await pool.query(`ALTER SEQUENCE hbb_person.patients_id_seq RESTART `);
+
+        await insertOperation(pool, {
+            firstName: 'maria',
+            lastName: 'palaria',
+            emailAddress: 'maria.pavelescu@maria.com',
+            homeAddress: 'str smariilor palariilor',
+            sex: 'female',
+            dateOfBirth: '17.08.2000'
+        });
     });
 
     after(async () => {
-        await server.stop();
+       await pool.query(`
+            DELETE FROM hbb_person.patients WHERE user_id=1`);
+
+       await server.stop();
     });
 
     it('given existing person > when calling get /person/:personId > should return valid response', async () => {
-        const payload = {
-            requestId: 1,
-            username: 'person123',
-            externalToken: 1
-        };
 
-        const response = await request(`http://localhost:${port}`).get('/person/1').send(payload);
+        const response = await axios.get(`http://localhost:${port}/person/1`);
+        const responsePayload = response.data as Person;
 
-        expect(response.status).to.be.equal(200);
-        expect(response.body).to.be.deep.equal({
-                personId: "1",
-                firstName: "Maria",
-                lastName: "Mihaila"
+        expect(responsePayload).to.be.deep.equal({
+            userId: 1,
+            details: {
+                dateOfBirth: "17.08.2000",
+                emailAddress: "maria.pavelescu@maria.com",
+                firstName: "maria",
+                homeAddress: "str smariilor palariilor",
+                id: "1",
+                lastName: "palaria",
+                sex: "female",
+                userId: 1
+            }
         });
     });
 });
