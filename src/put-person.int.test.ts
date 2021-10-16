2d0679a42ec5dd11319a517da04d244a09646dd1
Maria <maria.mihaila.17@gmail.com>
2021-10-16 11:55:11 +0300

    Fix edit person integration test

--- src/put-person.int.test.ts
+++ src/put-person.int.test.ts
@@ -1,9 +1,9 @@
 import {expect} from 'chai';
-import request from 'supertest';
+import axios from "axios";
 import {createWebServer} from "./create-web-server";
 
 
-describe('/person/edit', () => {
+describe('/person/1/edit', () => {
     let port: number;
     let server: { stop: () => Promise<unknown>, port: number, start: () => Promise<unknown> };
 
@@ -18,19 +18,33 @@ describe('/person/edit', () => {
         await server.stop();
     });
 
-    it('given non-existing person > when calling post /person/create > should return valid response', async () => {
+    it('given existing person > when calling put /person/create > should return valid response', async () => {
         const payload = {
-            requestId: 1,
-            username: 'person123',
-            externalToken: 1
+            firstName: 'maria',
+            lastName: 'palaria',
+            emailAddress: 'maria.palaria@maria.com',
+            homeAddress: 'str sfintilor',
+            sex: 'female',
+            dateOfBirth: '17.08.1995'
         };
 
-        const response = await request(`http://localhost:${port}`).put('/person/edit').send(payload);
+        const response = await axios.put(`http://localhost:${port}/person/1/edit`, payload);
 
         expect(response.status).to.be.equal(200);
-        expect(response.body).to.be.deep.equal({
-            firstName: "Razvan",
-            lastName: "Pavelescu"
+        expect(response.data).to.be.deep.equal({
+            userId: "1",
+            patientDetails: {
+                createdAt: "2021-10-16T05:27:52.198Z",
+                firstName: 'maria',
+                lastName: 'palaria',
+                emailAddress: 'maria.palaria@maria.com',
+                homeAddress: 'str sfintilor',
+                id: "1",
+                sex: 'female',
+                dateOfBirth: '17.08.1995',
+                userId: 1
+            },
+            message: `The user maria palaria with the id 1 was updated.`,
         });
     });
 });
