39b5bfbb2dfd3b3653efb06192cb109e1d678357
Maria <maria.mihaila.17@gmail.com>
2021-10-17 21:45:35 +0300

    Fix put person int test

--- src/put-person.int.test.ts
+++ src/put-person.int.test.ts
@@ -1,24 +1,44 @@
 import {expect} from 'chai';
 import axios from "axios";
 import {createWebServer} from "./create-web-server";
+import {insertOperation} from "./operations";
+import {connectToDb} from "./db-connect";
 
 
 describe('/person/1/edit', () => {
     let port: number;
     let server: { stop: () => Promise<unknown>, port: number, start: () => Promise<unknown> };
 
+    let pool = connectToDb();
+
     before(async () => {
         server = createWebServer();
         port = server.port;
 
         await server.start();
+
+        await pool.query(`ALTER SEQUENCE hbb_person.patients_user_id_seq RESTART `);
+        await pool.query(`ALTER SEQUENCE hbb_person.patients_id_seq RESTART `);
+
+        await insertOperation(pool, {
+            firstName: 'maria',
+            lastName: 'palarioara',
+            emailAddress: 'palariereste@palaria.ro',
+            homeAddress: 'str palariei',
+            sex: 'female',
+            dateOfBirth: '17.08.1995'
+        });
+
     });
 
     after(async () => {
+        await pool.query(`
+            DELETE FROM hbb_person.patients WHERE user_id=1`);
+
         await server.stop();
     });
 
-    it('given existing person > when calling put /person/create > should return valid response', async () => {
+    it('given existing person > when calling put /person/:userId/edit > should return valid response', async () => {
         const payload = {
             firstName: 'maria',
             lastName: 'palaria',
@@ -34,7 +54,6 @@ describe('/person/1/edit', () => {
         expect(response.data).to.be.deep.equal({
             userId: "1",
             patientDetails: {
-                createdAt: "2021-10-16T05:27:52.198Z",
                 firstName: 'maria',
                 lastName: 'palaria',
                 emailAddress: 'maria.palaria@maria.com',
